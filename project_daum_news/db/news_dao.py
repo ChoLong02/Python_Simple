5cd5dcd7ac4dcde41feadf3f4232f96f4624818b
ChoLong02 <sentilemon02@gmail.com>
2023-11-15 17:30:03 +0900

    Add pj_daum_movie

--- project_daum_news/db/news_dao.py
+++ project_daum_news/db/news_dao.py
@@ -13,5 +13,11 @@ from db.common.connection import conn_mongodb
 
 # 뉴스(제목, 본문, 날짜) 저장
 def add_news(data):
+    # 1.Connection
+    #  pymongo를 사용해서 → Python - MongoDB
+    #                     (IP, PORT, ID, PW)
+    # 2.SQL(명령)을 통해서 CRUD 작업 진행
+    # 3.결과 확인
+
     conn = conn_mongodb()  # 1.Connection
     conn.insert_one(data)  # 2.DB에 저장
